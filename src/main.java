f3b85a31fde1c282054670ff6ab8817202af9d09
Chinmayee0409 <cg3690@srmist.edu.in>
2026-04-15 10:30:39 +0530

    UC3 Generic Quantity Length Class

--- src/main.java
+++ src/main.java
@@ -1,41 +1,62 @@
-public class main {
+public class main{
 
-    static class Feet {
+    enum LengthUnit {
+        FEET(1.0),
+        INCH(1.0 / 12.0);
+
+        private final double conversionFactor;
+
+        LengthUnit(double conversionFactor) {
+            this.conversionFactor = conversionFactor;
+        }
+
+        public double toFeet(double value) {
+            return value * conversionFactor;
+        }
+    }
+
+    static class QuantityLength {
         private final double value;
+        private final LengthUnit unit;
 
-        public Feet(double value) {
+        public QuantityLength(double value, LengthUnit unit) {
+            if (unit == null) {
+                throw new IllegalArgumentException("Unit cannot be null");
+            }
             this.value = value;
+            this.unit = unit;
+        }
+
+        private double toBaseUnit() {
+            return unit.toFeet(value);
         }
 
         @Override
         public boolean equals(Object obj) {
             if (this == obj) return true;
             if (obj == null || getClass() != obj.getClass()) return false;
-            Feet feet = (Feet) obj;
-            return Double.compare(feet.value, value) == 0;
+            QuantityLength other = (QuantityLength) obj;
+            return Double.compare(this.toBaseUnit(), other.toBaseUnit()) == 0;
         }
     }
 
     public static void main(String[] args) {
 
-        // testEquality_SameValue
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(1.0);
-        System.out.println("Same Value Test: " + f1.equals(f2));
+        QuantityLength q1 =
+                new QuantityLength(1.0, LengthUnit.FEET);
 
-        // testEquality_DifferentValue
-        Feet f3 = new Feet(2.0);
-        System.out.println("Different Value Test: " + f1.equals(f3));
+        QuantityLength q2 =
+                new QuantityLength(12.0, LengthUnit.INCH);
 
-        // testEquality_NullComparison
-        System.out.println("Null Comparison Test: " + f1.equals(null));
+        QuantityLength q3 =
+                new QuantityLength(1.0, LengthUnit.INCH);
 
-        // testEquality_SameReference
-        Feet f4 = f1;
-        System.out.println("Same Reference Test: " + f1.equals(f4));
+        QuantityLength q4 =
+                new QuantityLength(1.0, LengthUnit.INCH);
 
-        // testEquality_NonNumericInput
-        String nonNumeric = "test";
-        System.out.println("Non Numeric Test: " + f1.equals(nonNumeric));
+        System.out.println("1 ft == 12 inch : " + q1.equals(q2));
+        System.out.println("1 inch == 1 inch : " + q3.equals(q4));
+        System.out.println("1 ft == 2 ft : " +
+                q1.equals(new QuantityLength(2.0, LengthUnit.FEET)));
     }
 }
