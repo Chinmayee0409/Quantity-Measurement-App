9379a566413f26c673550397b040c2f822ca8494
Chinmayee0409 <cg3690@srmist.edu.in>
2026-04-15 10:03:19 +0530

    Updated UC1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+    static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Feet feet = (Feet) obj;
+            return Double.compare(feet.value, value) == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // testEquality_SameValue
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+        System.out.println("Same Value Test: " + f1.equals(f2));
+
+        // testEquality_DifferentValue
+        Feet f3 = new Feet(2.0);
+        System.out.println("Different Value Test: " + f1.equals(f3));
+
+        // testEquality_NullComparison
+        System.out.println("Null Comparison Test: " + f1.equals(null));
+
+        // testEquality_SameReference
+        Feet f4 = f1;
+        System.out.println("Same Reference Test: " + f1.equals(f4));
+
+        // testEquality_NonNumericInput
+        String nonNumeric = "test";
+        System.out.println("Non Numeric Test: " + f1.equals(nonNumeric));
+    }
 }
